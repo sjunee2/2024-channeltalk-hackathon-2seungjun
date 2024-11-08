a609c80c84279a9961c0b6b0f7842d77c53020ab
haionaz <samhsj@naver.com>
2024-11-09 05:36:50 +0900

    feat:api

--- wam/src/pages/CalendarPage/index.tsx
+++ wam/src/pages/CalendarPage/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useFilterStore } from '../../store/filter'
 import Filter from '../../componenets/Filter'
 import Calendar from '../../componenets/Calendar'
@@ -8,23 +8,11 @@ import { Task, User } from '../../types/task'
 import { FaCalendarAlt, FaBars } from 'react-icons/fa'
 import styled from 'styled-components'
 import LeaderBoard from '../LeaderBoard'
+import { BASE_URL } from '../../secret'
+import { getWamData } from '../../utils/wam'
 
 const CalendarPage = () => {
   const [taskData, setTaskData] = useState<Task[]>([])
-  useEffect(() => {
-    setTaskData([
-      {
-        id: 1,
-        status: 'proposal',
-        title: 'title',
-        contents: 'contents',
-        startDate: '2021-09-01',
-        endDate: '2024-09-10',
-        role: 'owner',
-        assignUser: ['123'],
-      },
-    ])
-  }, [])
 
   const { status, role, assignUser } = useFilterStore()
 
@@ -35,7 +23,7 @@ const CalendarPage = () => {
   // const chatTitle = useMemo(() => getWamData('chatTitle') ?? '', [])
 
   // const appId = useMemo(() => getWamData('appId') ?? '', [])
-  // const channelId = useMemo(() => getWamData('channelId') ?? '', [])
+  const channelId = useMemo(() => getWamData('channelId') ?? '', [])
   // const managerId = useMemo(() => getWamData('managerId') ?? '', [])
   // const userId = useMemo(() => getWamData('userId') ?? '', [])
   // const message = useMemo(() => getWamData('message') ?? '', [])
@@ -43,128 +31,6 @@ const CalendarPage = () => {
   // const chatType = useMemo(() => getWamData('chatType') ?? '', [])
   // // const broadcast = useMemo(() => Boolean(getWamData('broadcast') ?? false), [])
   // const rootMessageId = useMemo(() => getWamData('rootMessageId'), [])
-
-  // const handleSend = useCallback(
-  //   async (sender: string): Promise<void> => {
-  //     if (chatType === 'group') {
-  //       switch (sender) {
-  //         case 'bot':
-  //           await callFunction(appId, 'sendAsBot', {
-  //             input: {
-  //               groupId: chatId,
-  //               broadcast,
-  //               rootMessageId,
-  //             },
-  //           })
-  //           break
-  //         case 'manager':
-  //           await callNativeFunction('writeGroupMessageAsManager', {
-  //             channelId,
-  //             groupId: chatId,
-  //             rootMessageId,
-  //             broadcast,
-  //             dto: {
-  //               plainText: message,
-  //               managerId,
-  //             },
-  //           })
-  //           break
-  //         default:
-  //           // NOTE: should not reach here
-  //           console.error('Invalid message sender')
-  //       }
-  //     } else if (chatType === 'directChat') {
-  //       // FIXME: Implement
-  //     } else if (chatType === 'userChat') {
-  //       // FIXME: Implement
-  //     }
-  //   },
-  //   [
-  //     appId,
-  //     broadcast,
-  //     channelId,
-  //     chatId,
-  //     chatType,
-  //     managerId,
-  //     message,
-  //     rootMessageId,
-  //   ]
-  // )
-
-  // useEffect(() => {
-  //   // callFunction(appId, '')
-  //   // const data = async () => {
-  //   //   await callFunction(appId, 'sendAsBot', {
-  //   //     input: {
-  //   //       groupId: chatId,
-  //   //       broadcast,
-  //   //       rootMessageId,
-  //   //     },
-  //   //   })
-  //   // }();
-  //   setTaskData([
-  //     {
-  //       id: 1,
-  //       status: 'proposal',
-  //       title: `${channelId}`,
-  //       contents: 'channelId',
-  //       startDate: '2021-09-01',
-  //       endDate: '2024-09-10',
-  //       role: 'owner',
-  //       assignUser: ['123'],
-  //     },
-  //     {
-  //       id: 2,
-  //       status: 'progress',
-  //       title: `${appId}`,
-  //       contents: 'appId',
-  //       startDate: '2021-09-01',
-  //       endDate: '2024-09-10',
-  //       role: 'owner',
-  //       assignUser: ['123'],
-  //     },
-  //     {
-  //       id: 3,
-  //       status: 'progress',
-  //       title: `${userId}`,
-  //       contents: 'userId',
-  //       startDate: '2021-09-01',
-  //       endDate: '2024-09-10',
-  //       role: 'owner',
-  //       assignUser: ['123'],
-  //     },
-  //     {
-  //       id: 4,
-  //       status: 'progress',
-  //       title: `${managerId}`,
-  //       contents: 'managerId',
-  //       startDate: '2021-09-01',
-  //       endDate: '2024-09-10',
-  //       role: 'owner',
-  //       assignUser: ['123'],
-  //     },
-  //     {
-  //       id: 5,
-  //       status: 'progress',
-  //       title: `${chatType}`,
-  //       contents: 'chatType',
-  //       startDate: '2021-09-01',
-  //       endDate: '2024-09-10',
-  //       role: 'owner',
-  //       assignUser: ['123'],
-  //     },
-  //     {
-  //       id: 6,
-  //       status: 'progress',
-  //       title: `${rootMessageId}`,
-  //       contents: 'rootMessageId',
-  //       startDate: '2021-09-01',
-  //       endDate: '2024-09-10',
-  //       role: 'owner',
-  //       assignUser: ['123'],
-  //     },
-  //   ])
-  // }, [appId, channelId, managerId, chatType, rootMessageId, userId])
 
   const filterTasks = (
     tasks: Task[],
@@ -181,51 +47,29 @@ const CalendarPage = () => {
   }
   const filteredData = filterTasks(taskData, status, role, assignUser)
 
-  // const filteredData = useMemo(() => tas)
-
-  // const roleData: Role[] = []
-  const userData: User[] = [
-    {
-      id: '123124',
-      avatarUrl: 'https://avatars.githubusercontent.com/u/58812281?v=4',
-      role: '소유자',
-      name: 'halion',
-      totalTasks: 6,
-      completedTasks: 5,
-    },
-    {
-      id: '123124',
-      avatarUrl: 'https://avatars.githubusercontent.com/u/58812281?v=4',
-      role: '소유자',
-      name: '2sj',
-      totalTasks: 9,
-      completedTasks: 8,
-    },
-    {
-      id: '123124',
-      avatarUrl: 'https://avatars.githubusercontent.com/u/58812281?v=4',
-      role: '소유자',
-      name: '2ewhaejklrsj',
-      totalTasks: 6,
-      completedTasks: 10,
-    },
-    {
-      id: '123124',
-      avatarUrl: 'https://avatars.githubusercontent.com/u/58812281?v=4',
-      role: '소유자',
-      name: 'adsf',
-      totalTasks: 6,
-      completedTasks: 12,
-    },
-  ]
-  const myData: User = {
+  const [userData, setUserData] = useState<User[]>([])
+  const [myData, setMyData] = useState<User>({
     id: '123124',
     avatarUrl: 'https://avatars.githubusercontent.com/u/58812281?v=4',
     role: '소유자',
     name: '2sj',
     totalTasks: 6,
     completedTasks: 5,
-  }
+  })
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/functions/task/all-user`)
+      .then((res) => res.json())
+      .then((data) => setUserData(data))
+    fetch(`${BASE_URL}/functions/task/user/{userId}`)
+      .then((res) => res.json())
+      .then((data) => setMyData(data))
+    fetch(`${BASE_URL}/functions/task/${channelId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setTaskData(data)
+      })
+  }, [channelId])
 
   return (
     <div
