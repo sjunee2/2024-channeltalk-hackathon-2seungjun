eb36dbdea5f8ab37c5b8de24748606f41c2da892
haionaz <samhsj@naver.com>
2024-11-08 19:36:01 +0900

    fe init

--- wam/src/App.tsx
+++ wam/src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { AppProvider, type ThemeName } from '@channel.io/bezier-react'
+
+import { isMobile } from './utils/userAgent'
+import { getWamData } from './utils/wam'
+import Send from './pages/Send'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState<ThemeName>('light')
+
+  useEffect(() => {
+    const appearance = getWamData('appearance')
+    setTheme(appearance === 'dark' ? 'dark' : 'light')
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <AppProvider themeName={theme}>
+      <div style={{ padding: isMobile() ? '16px' : '0 24px 24px 24px' }}>
+        <Send />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </AppProvider>
   )
 }
 
