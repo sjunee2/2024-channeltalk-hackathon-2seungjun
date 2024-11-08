1fdb9f9927b9ca4f1e138898a0f1f45986edd156
Devheun <dudwo5600@korea.ac.kr>
2024-11-09 05:22:35 +0900

    cors 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
