0ab796c7e2c9d50bb9f6b2e07916e22c9b16895b
haionaz <samhsj@naver.com>
2024-11-08 20:04:50 +0900

    chore

--- wam/src/App.tsx
+++ wam/src/App.tsx
@@ -4,6 +4,11 @@ import { AppProvider, type ThemeName } from '@channel.io/bezier-react'
 import { isMobile } from './utils/userAgent'
 import { getWamData } from './utils/wam'
 import Send from './pages/Send'
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css'
+
+import { MantineProvider } from '@mantine/core'
 
 function App() {
   const [theme, setTheme] = useState<ThemeName>('light')
@@ -15,9 +20,11 @@ function App() {
 
   return (
     <AppProvider themeName={theme}>
-      <div style={{ padding: isMobile() ? '16px' : '0 24px 24px 24px' }}>
-        <Send />
-      </div>
+      <MantineProvider>
+        <div style={{ padding: isMobile() ? '16px' : '0 24px 24px 24px' }}>
+          <Send />
+        </div>
+      </MantineProvider>
     </AppProvider>
   )
 }
